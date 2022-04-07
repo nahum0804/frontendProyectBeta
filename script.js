fb7d2ea80191c1d92538759c1168efd6be823854
Abraham Carvajal <acarvajal@saatvamattress.com>
2022-04-06 20:42:48 -0600

    Ensure dom is loaded before attaching the events

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const tuno = document.querySelector("#t1");
+  const tdos = document.querySelector("#t2");
+  const ttres = document.querySelector("#t3");
 
-const tuno = document.querySelector("#t1");
-const tdos = document.querySelector("#t2");
-const ttres = document.querySelector("#t3");
-
-tuno.addEventListener('click', () => {
-  console.log("entrando");
-});
-tdos.addEventListener('click', redirectionDos);
-ttres.addEventListener('click', redirectionTres);
+  tuno.addEventListener('click', () => {
+    console.log("Titoooo");
+  });
+  tdos.addEventListener('click', redirectionDos);
+  ttres.addEventListener('click', redirectionTres);
 
 
-function redirectionDos() {}
-function redirectionTres() {}
+  function redirectionDos() {}
+  function redirectionTres() {}
+
+}, false);
